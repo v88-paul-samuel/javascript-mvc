415006cff502e07a0e973153eb25accedb4cd911
v88-paul-samuel <placap@village88.com>
2022-12-07 08:37:26 +0800

    validator fixed

--- helpers/validator.js
+++ helpers/validator.js
@@ -4,13 +4,13 @@ export default {
     login: [
         body("email_address").trim().notEmpty().withMessage("Email Address is required")
             .isEmail().withMessage("Must be a valid Email").normalizeEmail().escape(),
-        body("password").trim().isLength({min:3}).withMessage("Invalid Password Entry").escape(),
+        body("password").trim().isLength({min:3}).withMessage("Password must have a minimum of 3 Characters").escape(),
     ],
     register: [
         body("first_name").trim().notEmpty().withMessage("First Name is required").escape(),
         body("last_name").trim().notEmpty().withMessage("Last Name is required").escape(),
         body("email_address").trim().notEmpty().withMessage("Email address is required")
             .isEmail().withMessage("Must be a valid Email").normalizeEmail().escape(),
-        body("password").trim().isLength({min:3}).withMessage("Invalid Password Entry").escape()
+        body("password").trim().isLength({min:3}).withMessage("Password must have a minimum of 3 Characters").escape()
     ]
 };
