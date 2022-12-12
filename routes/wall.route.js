62552df176801e0074316780ff8409a6b89d0e72
v88-paul-samuel <placap@village88.com>
2022-12-12 10:26:37 +0800

    Revised the starting point

--- routes/wall.route.js
+++ routes/wall.route.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import ViewController from "../controllers/view.controller.js";
+import UserController from "../controllers/users.controller.js";
 import validateUser from "../helpers/validator.js";
 const WallRoute = Router();
 
 
 WallRoute.get("/", (req, res) => { new ViewController(req, res).homepage(); });
 WallRoute.get("/homepage", (req, res) => { new ViewController(req, res).homepage(); });
-WallRoute.get("/logoff", (req, res) => { new ViewController(req, res).logoff(); });
-WallRoute.post("/login", validateUser.login, (req, res) => { new ViewController(req, res).login(); })
-WallRoute.post("/register", validateUser.register,  (req, res) => { new ViewController(req, res).register(); })
+WallRoute.get("/logoff", (req, res) => { new UserController(req, res).logoff(); });
+WallRoute.post("/login", validateUser.login, (req, res) => { new UserController(req, res).login(); })
+WallRoute.post("/register", validateUser.register,  (req, res) => { new UserController(req, res).register(); })
 
 export default WallRoute;
