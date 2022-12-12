a2f3e2fdcabdfbcb9d95950c1fbbba4eb507905c
v88-paul-samuel <placap@village88.com>
2022-12-12 17:28:09 +0800

    removed unneeded routes

--- routes/wall.route.js
+++ routes/wall.route.js
@@ -13,7 +13,7 @@ WallRoute.get("/wall", (req, res) => { new ViewController(req, res).wall(); })
 WallRoute.get("/logoff", (req, res) => { new UsersController(req, res).logoff(); });
 WallRoute.post("/login", ValidateUser.login, (req, res) => { new UsersController(req, res).login(); })
 WallRoute.post("/register", ValidateUser.register,  (req, res) => { new UsersController(req, res).register(); })
-WallRoute.post("/postMessage", ValidateUser.post_message, (req, res) => { 
+WallRoute.post("/postMessage", (req, res) => { 
     try{
         new WallsController(req, res).postMessage(); 
     }
@@ -21,7 +21,7 @@ WallRoute.post("/postMessage", ValidateUser.post_message, (req, res) => {
         res.json({status : false, result : {}, error : error.message});
     }
 });
-WallRoute.post("/postComment", ValidateUser.post_comment, (req, res) => { 
+WallRoute.post("/postComment", (req, res) => { 
     try{
         new WallsController(req, res).postComment();
     }
